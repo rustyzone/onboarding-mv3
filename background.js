e4b6b556e27f7dc4092fd14d72dd218401e0dd9a
rustyzone <66030523+rustyzone@users.noreply.github.com>
2021-03-03 15:14:29 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 
-
+try{
 //On first install open onboarding
 chrome.runtime.onInstalled.addListener(r => {
   if(r.reason == 'install'){
@@ -14,12 +14,21 @@ chrome.runtime.onInstalled.addListener(r => {
 
 //ON page change
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // read changeInfo data and do something with it (like read the url)
-  if (changeInfo.url) {
-    //if have all_urls permissions...
-    chrome.scripting.executeScript({
-      //  ^^ This line works but according to the offical docs it should be chrome.scripting.executeScript
-      files: ['contentScript.js']
-    });
-  }
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+    const tabId = tabs[0].id;
+    // read changeInfo data and do something with it (like read the url)
+    if (changeInfo.url) {
+      //if have all_urls permissions...
+      chrome.scripting.executeScript({
+        //  ^^ This line works but according to the offical docs it should be chrome.scripting.executeScript
+        files: ['contentScript.js'],
+        target: {tabId: tabId}
+      });
+    }
+  });
 });
+
+
+}catch(e){
+  console.log(e);
+}
